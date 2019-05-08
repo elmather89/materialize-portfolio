2f19534ce86c3f1f44d6d79368cdfe98fc1c55fc
Sansevieria <elizabethleighmather@gmail.com>
2019-05-08 14:34:58 -0500

    added resume to floating button

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // initialize hero slider
     $('.slider').slider();
@@ -6,15 +6,19 @@ $(document).ready(function(){
     // initialize floating button
     $('.fixed-action-btn').floatingActionButton();
 
-    $("#write-btn").on("click", function() {
+    $("#resume-btn").on("click", function () {
+        window.open("https://static1.squarespace.com/static/584ae46c20099e929d2247d6/t/5cd32dfbf9345f0001a02ff3/1557343739392/ElizabethMatherCV_2019.pdf");
+    });
+
+    $("#write-btn").on("click", function () {
         window.open('mailto:elizabethleighmather@gmail.com?subject=Hello&body=body');
     });
 
-    $("#linkedin-btn").on("click", function() {
+    $("#linkedin-btn").on("click", function () {
         window.open("https://www.linkedin.com/in/liz-mather-kansascity");
     });
 
-    $("#github-btn").on("click", function() {
+    $("#github-btn").on("click", function () {
         window.open("https://github.com/elmather89");
     })
 });
